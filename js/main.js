e1abb3fcbdabefa2caca7831dfcd59de7c59a686
Axell <becheralexandru@gmail.com>
2021-05-31 15:36:06 +0300

    Add Cookies

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@ var navButton = document.getElementById('nav-button');
 function toggleMenu() {
     navButton.classList.toggle('active');
 }
+
+$(document).ready(function(){   
+    setTimeout(function () {
+        $("#cookieConsent").fadeIn(200);
+     }, 3000);
+    $("#closeCookieConsent, .cookieConsentOK").click(function() {
+        $("#cookieConsent").fadeOut(200);
+    }); 
+}); 
